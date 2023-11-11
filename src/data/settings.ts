daa3507c36fa94f443a5ec71bd96a165be63e095
gauravxshukla <gauravvvshukla@gmail.com>
2023-11-11 17:11:45 +0530

    SSR-SSG-ISR-Post-added

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,7 +1,6 @@
 export const settings = {
-  site: 'https://blog.gauravxshukla.me',
+  site: 'https://blog.gauravxshukla.me/',
   name: 'Gaurav Shukla\'s Blog',
   title: 'Gaurav Shukla - Blog',
   description: 'Site description',
-  showLittleSticksPlug: true,
 }
